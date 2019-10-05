ead5a6e0cab924f874ca41b842b7751d60719e3d
Christopher Mark Gore <cgore@cgore.com>
2019-10-05 17:51:00 -0500

    next-boolean

--- src/mersenne_twister/core.clj
+++ src/mersenne_twister/core.clj
@@ -1,1 +1,10 @@
-(ns mersenne-twister.core)
+(ns mersenne-twister.core
+  (:import
+   ;; http://commons.apache.org/proper/commons-math/javadocs/api-3.6.1/org/apache/commons/math3/random/MersenneTwister.html
+   [org.apache.commons.math3.random MersenneTwister]))
+
+(defn generator []
+  (new MersenneTwister))
+
+(defn next-boolean [generator]
+  (.nextBoolean ^MersenneTwister generator))
