987092997693258ee63069961eadd1e4b1e85471
Christopher Mark Gore <cgore@cgore.com>
2019-10-05 23:13:54 -0500

    Fixing the docstring for next-int

--- src/mersenne_twister/core.clj
+++ src/mersenne_twister/core.clj
@@ -49,8 +49,22 @@
    (.nextGaussian ^MersenneTwister generator)))
 
 (defn next-int
-  "Returns the next pseudorandom, uniformly distributed int value between 0.0
-  and 1.0 from this random number generator's sequence."
+  "Returns the next pseudorandom, uniformly distributed int value from this
+  random number generator's sequence. All 232 possible int values should be
+  produced with (approximately) equal probability.
+
+  This optionally takes a value `n`, such that it returns a pseudorandom,
+  uniformly distributed int value between 0 (inclusive) and the specified
+  value (exclusive), drawn from this random number generator's sequence.
+
+  When `n` is specified:
+
+   * If n is a power of 2, this method returns (int) ((n * (long) next(31)) >> 31).
+
+   * If n is not a power of 2, what is returned is next(31) % n with next(31)
+     values rejected (i.e. regenerated) until a value that is larger than the
+     remainder of Integer.MAX_VALUE / n is generated. Rejection of this initial
+     segment is necessary to ensure a uniform distribution."
   ([]
    (.nextInt ^MersenneTwister @generator))
   ([thing]
